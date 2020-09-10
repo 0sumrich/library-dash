29270cef4530cdb346039b8fcbcc031ee2f379ef
Richard Lynch <lynchardrich@gmail.com>
2020-09-10 19:38:22 +0100

    side navvin

--- src/components/sideNav.js
+++ src/components/sideNav.js
@@ -6,14 +6,23 @@ import ListItemText from '@material-ui/core/ListItemText';
 import makeId from '../helper/makeId'
 import Scrollspy from 'react-scrollspy'
 
+const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      position: 'fixed',
+      overflow: 'hidden'
+    },
+  }));
+
 function ListItemLink(props) {
     return <ListItem button component="a" {...props} />;
 }
 
 function SideNav({ titles }) {
+    const classes = useStyles()
     const ids = titles.map(o => makeId(o))
     return (
-        <List>
+        <List className={classes.root}>
             <Scrollspy items={ids} currentClassName="is-current">
                 {titles.map((title, i) => <ListItemLink href={`#${ids[i]}`}><ListItemText primary={title}></ListItemText></ListItemLink>)}
             </Scrollspy>
